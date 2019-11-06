74cd7858681a64f5f3a5058a2d69748b17bcaaf9
ruiz-m <ruiz.m@ufl.edu>
2019-11-06 17:30:14 -0500

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 #include<vector>
 #include <cstring>
-#include"Image.h"
-#include"net.h"
+#include"../header/Image.h"
+#include"../header/net.h"
 
 using namespace std;
 
@@ -14,70 +14,64 @@ char* createArray(string s);
 
 int main()
 {
-    char* cfile = createArray("./fotos/orange1.bmp");
-    char* cfile2 = createArray("./fotos/apple.bmp");
-    char* cfile3 = createArray("./fotos/orange2.bmp");
-    char* cfile4 = createArray("./fotos/appleii.bmp");
-    //char* cfile5 = createArray("./fotos/banana.bmp");
+    char* cfile = createArray("./pictures/orange1.bmp");
+    char* cfile2 = createArray("./pictures/apple.bmp");
+    char* cfile3 = createArray("./pictures/orange2.bmp");
+    char* cfile4 = createArray("./pictures/appleii.bmp");
+    char* cfile5 = createArray("./pictures/apple3.bmp");
 
-    Image X(cfile);
-    Image O(cfile2);
+    Image orange(cfile);
+    Image apple(cfile2);
     Image orange2(cfile3);
     Image apple2(cfile4);
-    //Image banana(cfile5);
+    Image apple3(cfile5);
 
     delete[] cfile;
     delete[] cfile2;
     delete[] cfile3;
     delete[] cfile4;
-    //delete[] cfile5;
+    delete[] cfile5;
 
     vector<int> top = {2};
     vector<float> expected = {0, 0};
-    Net net(28, 28, top, X.width, O.height);
-
-    
-    //printConv(net);
-
-    /*net.feedfoward(orange);
-    printOutput(net);
-
-    net.feedfoward(apple);
-    printOutput(net);*/
+    Net net(28, 28, top, orange.width, orange.height);
 
     cout << "Training" << endl;
     for(unsigned int i = 0; i < 100; i++)
     {
         expected.clear();
         expected = {0, 1};
-        net.feedfoward(X);
+        net.feedfoward(orange);
         net.backProp(expected);
         net.updateWeights();
 
         expected.clear();
         expected = {1, 0};
-        net.feedfoward(O);
+        net.feedfoward(apple);
         net.backProp(expected);
         net.updateWeights();
     }
     //printConv(net);
 
-    net.feedfoward(X);
+    cout << "Orange1 Expected: 0, 1" << endl;
+    net.feedfoward(orange);
     printOutput(net);
 
-    net.feedfoward(O);
+    cout << "Apple1 Expected: 1, 0" << endl;
+    net.feedfoward(apple);
     printOutput(net);
 
+    cout << "Orange2 Expected: 0, 1" << endl;
     net.feedfoward(orange2);
     printOutput(net);
 
+    cout << "Apple2 Expected: 1, 0" << endl;
     net.feedfoward(apple2);
     printOutput(net);
-
-    //printOutput(net);
-
-    /*net.feedfoward(banana);
-    printOutput(net);*/
+    
+    cout << "Apple3 Expected: 1, 0" << endl;
+    net.feedfoward(apple3);
+    printOutput(net);
     cout << endl;
     return 0;
 }
@@ -102,15 +96,11 @@ void printFilter(Net &net)
 void printOutput(Net &net)
 {
     cout << "Output " << endl;
-    for(unsigned int i = 0; i < net.getFLayers().size(); i++)
+    for(unsigned int j = 0; j < net.getFLayers()[net.getFLayers().size() - 1].size(); j++)
     {
-        for(unsigned int j = 0; j < net.getFLayers()[i].size(); j++)
-        {
-            cout << net.getFLayers()[i][j].getOutput() << " ";
-        }
-        cout << endl;
+        cout << net.getFLayers()[net.getFLayers().size() - 1][j].getOutput() << " ";
     }
-    cout << endl;
+    cout << "\n" << endl;
 }
 
 void printConv(Net &net)
